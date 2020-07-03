cfd420a1e04fde78b1bbc8e61d6caeceb617b018
horaciogomezmx <horaciogomezmx@gmail.com>
2020-07-03 16:57:33 -0500

    mas

---
+++ arreglo.py
@@ -0,0 +1,4 @@
+arreglo = [1, 2, 3]
+x = 1
+resultado = arreglo[x] * 2
+print(resultado)
---
+++ selecion.py
@@ -0,0 +1,10 @@
+selecion = imput("Ingresa una opción")
+if seleccion == 'a'
+    print("Elegiste la opcion a")
+elif seleccion == 'b'
+    print("Elegiste la opcion b")
+elif seleccion in ['a' , 'e', 'i',  'o', 'u']:
+    print("Elegiste una vocal")
+else:
+    print("Elegiste otra opcion")
+    
