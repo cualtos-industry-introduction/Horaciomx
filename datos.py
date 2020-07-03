f9c08561ae42b4490314eb952bd93b484de33583
horaciogomezmx <horaciogomezmx@gmail.com>
2020-07-03 16:26:05 -0500

    Add files via upload

---
+++ datos.py
@@ -0,0 +1,26 @@
+tupla = 1, 2, 3, 4, 'Hola', 3.2
+print(tupla[:])
+#tupla[0] = 3
+diccionario ={'nombre': 'Juan', 'apellido': 'Lopez'}
+usuario = {'nombre': 'Pedro', 'apellido': 'sanchez'}
+lista = [diccionario, usuario]
+print(diccionario['nombre'])
+print(lista[1:2])
+for registro in lista:
+    print(registro)
+
+
+usuario['correo'] = "abc@correo.com"
+
+#for registro 
+
+datos ={}
+dato['nombre'] = input("ingresa en uno nombre:")
+dato['direccion'] = input("ingresa la direccion:")
+dato['correo'] = input("ingresa en correo:")
+
+print(lista[0]['nombre'])
+for llave, valor in usuarios.items():
+    print(llave, valor)
+
+lista.append(dato)
